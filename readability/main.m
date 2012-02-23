eab8e8fa4209c11d8c6d00f3b6f21ac46a9caa36
Jan <github@geheimwerk.de>
2012-02-23 18:28:08 +0100

    Initial commit

---
+++ readability/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  readability
+//
+//  Created by Jan on 23.02.12.
+//  Copyright (c) 2012 geheimwerk.de. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+	
+    return 0;
+}
+
