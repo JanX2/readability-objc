6523ce27edf05f6ad8a142e4d69e80409f5b164a
Jan <github@geheimwerk.de>
2012-02-23 19:03:16 +0100

    Basic CLI.

--- readability/main.m
+++ readability/main.m
@@ -13,11 +13,54 @@ int main(int argc, const char * argv[])
 
 	@autoreleasepool {
 	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
+		NSUserDefaults *args = [NSUserDefaults standardUserDefaults];	
+		
+		NSString *url = [args stringForKey:@"url"];
+		NSString *verboseString = [args stringForKey:@"verbose"];
+		NSString *output = [args stringForKey:@"output"];
+		
+		BOOL verbose = [verboseString isEqualToString:@"YES"];
+		
+		BOOL isPath;
+		
+		if (url == nil) {
+#if 0
+			NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+			const char *executablePath = [[arguments objectAtIndex:0] 
+										  fileSystemRepresentation];
+#endif
+			fprintf(stderr, "readability 0.1\nUsage: \nreadability -url URL [-verbose YES|NO] -output FILE \n");
+			
+			return EXIT_FAILURE;
+		}
+		
+		if ([url hasPrefix:@"http://"]) {
+			isPath = NO;
+		} else {
+			isPath = YES;
+		}
+		
 	    
+		NSString *result = @"";
+		
+		
+		if (output == nil) {
+			fprintf(stdout, "%s\n", [result UTF8String]);
+		}
+		else {
+			NSError *error = nil;
+			BOOL OK = [result writeToFile:result 
+							   atomically:YES 
+								 encoding:NSUTF8StringEncoding 
+									error:&error];
+			
+			if (!OK && verbose) {
+				NSLog(@"\n%@", error);
+			}
+		}
+		
 	}
 	
-    return 0;
+	return EXIT_SUCCESS;
 }
 
